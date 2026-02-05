79faeff39f9cbc9059eed4fd2c7e27dc55156498
yadavhappy4068-collab <yadavhappy4068@gmail.com>
2026-02-05 15:24:20 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import NetflixSeries from "./components/NetiflixSeries";
+import "./App.css";
+
+
+const App = () => {
+  return (
+    <>
+      <NetflixSeries />
+    </>
+  );
+};
+
+export default App;
