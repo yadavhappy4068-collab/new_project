a2d993c8b0f53ac37228434d8ff5949725bb807b
yadavhappy4068-collab <yadavhappy4068@gmail.com>
2026-02-23 11:26:52 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,15 +1,51 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import  { Movie } from "./pages/Movies";
+import { Contact, contactData } from "./pages/Contact";
+import AppLayout from "./componets/layout/Applayout";
+import { ErrorPage } from "./pages/Error";
+import "./App.css"
+import { getMoviesData } from "./API/GetAPIData";
+import  {MoviesDetails}  from "./componets/UI/MoviesDetails";
+import { getMovieDetails } from "./API/GetMoviesDetails";
+
+const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <AppLayout />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/about",
+          element: <About />,
+        },
+        {
+          path: "/Movie",
+          element: <Movie />,
+          loader: getMoviesData,
+        },
+        {
+        path: "/movie/:movieID",
+        element: <MoviesDetails />,
+        loader: getMovieDetails,
+        },
+        {
+          path: "/contact",
+          element: <Contact />,
+          action: contactData,
+        },
+      ],
+    },
+  ]);
 
 
-import { ReactMemo } from "./hooks/memo/first"
-
-
-function App() {
-  return (
-    <>
-    <ReactMemo/>  
-    </>
-  );
-}
+  return <RouterProvider router={router} />;
+};
 
 export default App;
-
